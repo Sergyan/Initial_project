bc94977fcfcb6ae7324471e552d9ac23afac98f6
Sergyan <sergio.rubio.osorio1994@gmail.com>
2026-09-13 14:41:15 +0200

    Initial STM32 application structure

---
+++ Core/Src/main.c
@@ -0,0 +1,323 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+typedef enum
+{
+  APP_STATE_INIT = 0,
+  APP_STATE_WAIT_CLOCK,
+  APP_STATE_PROCESS_CLOCK,
+  APP_STATE_ERROR
+} AppState;
+
+typedef enum
+{
+  CLOCK_STATUS_READY = 0,
+  CLOCK_STATUS_BUSY,
+  CLOCK_STATUS_ERROR
+} ClockStatus;
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define CLOCK_PERIOD_MS 1000U
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+static volatile uint8_t button_event;
+static AppState app_state;
+static uint32_t next_clock_tick;
+static uint32_t clock_value_ms;
+static ClockStatus clock_status;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+static void board_init(void);
+static void app_run(void);
+__weak ClockStatus clock_query(uint32_t *time_ms);
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+static void board_init(void)
+{
+  BSP_LED_Init(LED2);
+  BSP_LED_Off(LED2);
+  BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
+}
+
+__weak ClockStatus clock_query(uint32_t *time_ms)
+{
+  *time_ms = HAL_GetTick();
+  return CLOCK_STATUS_READY;
+}
+
+static void app_run(void)
+{
+  uint32_t now = HAL_GetTick();
+
+  switch (app_state)
+  {
+    case APP_STATE_INIT:
+      next_clock_tick = now;
+      app_state = APP_STATE_WAIT_CLOCK;
+      break;
+
+    case APP_STATE_WAIT_CLOCK:
+      if (button_event != 0U || now >= next_clock_tick)
+      {
+        button_event = 0U;
+        clock_status = clock_query(&clock_value_ms);
+
+        if (clock_status == CLOCK_STATUS_READY)
+        {
+          app_state = APP_STATE_PROCESS_CLOCK;
+        }
+        else if (clock_status == CLOCK_STATUS_BUSY)
+        {
+          next_clock_tick = now + CLOCK_PERIOD_MS;
+        }
+        else if (clock_status == CLOCK_STATUS_ERROR)
+        {
+          app_state = APP_STATE_ERROR;
+        }
+      }
+      break;
+
+    case APP_STATE_PROCESS_CLOCK:
+      BSP_LED_Toggle(LED2);
+      next_clock_tick = now + CLOCK_PERIOD_MS;
+      app_state = APP_STATE_WAIT_CLOCK;
+      break;
+
+    case APP_STATE_ERROR:
+      BSP_LED_Toggle(LED2);
+      next_clock_tick = now + CLOCK_PERIOD_MS;
+      app_state = APP_STATE_WAIT_CLOCK;
+      break;
+
+    default:
+      app_state = APP_STATE_INIT;
+      break;
+  }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+  board_init();
+  app_state = APP_STATE_INIT;
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    app_run();
+    __WFI();
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : USART_TX_Pin USART_RX_Pin */
+  GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LED_GREEN_Pin */
+  GPIO_InitStruct.Pin = LED_GREEN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_GREEN_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  if (GPIO_Pin == USER_BUTTON_PIN)
+  {
+    button_event = 1U;
+  }
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Initial_project
-Initial Firmware for STM32F446RE: Real-Time State Machine.
+
+Firmware base for the STM32F446RE NUCLEO board.
+
+## Current application flow
+
+1. CubeMX initializes the HAL, clock and GPIO peripherals.
+2. `board_init()` initializes the NUCLEO LED on PA5 and the user button.
+3. The application state machine reads the clock and processes the result.
+4. The main loop enters `WFI` between events and timer ticks.
+
+## Clock integration
+
+`clock_query()` is a weak hook in `Core/Src/main.c`. The default implementation
+returns the elapsed milliseconds from `HAL_GetTick()`. The `.ioc` does not yet
+configure the STM32 RTC, so a board-specific RTC implementation can replace the
+hook and return `CLOCK_STATUS_BUSY` while the clock is being initialized, then
+`CLOCK_STATUS_READY` when the value is available.
+
+## Project
+
+Open `EWARM/Project.eww` with IAR Embedded Workbench for ARM. The CubeMX source
+configuration is kept in `Initial_project.ioc`.
